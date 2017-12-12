a94a3a1149ca08fee3bb325912978401bf639794
John Coote <john_coote@rocketmail.com>
2017-12-12 21:23:45 +1100

    first reactions

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Search from './Search';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <h1>CBJ Burning Airlines - Front End Page</h1>
+        <Search />
       </div>
     );
   }
---
+++ src/components/Search.js
@@ -0,0 +1,18 @@
+import React, {PureComponent as Component} from 'react'
+
+
+// const SERVER_URL = "http://593d3fe7.ngrok.io/secrets.json"
+
+class Search extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <h2>Search Page under construction...</h2>
+      </div>
+    )
+  }
+}
+
+export default Search
